14ec8d8d9b166ddb5c39f7a80e8270ba0688cff8
Thiago Toyomoto <github@thiagotoyomoto.com>
2023-10-22 19:20:25 -0300

    fix: Corrigir os imports

--- App.js
+++ App.js
@@ -1,39 +1,38 @@
-import { StyleSheet, Dimensions, View, SafeAreaView, Platform} from 'react-native';
-import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
-import Login from './src/Login';
-import Register from './src/Register';
-import RegisterData from './src/RegisterData';
-import NavBar from './src/NavBar';
-import theme from './styles/LoginStyle';
-import Header from './src/Header';
-import WarningModal from './src/WarningModal';
-import Profile from './src/Profile';
+import { StyleSheet, Dimensions, View, SafeAreaView, Platform } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
+import { ProfileScreen } from './src/screens';
+import { Header, NavBar } from './src/components';
+
+import theme from './src/styles/LoginStyle';
 
 const winHeight = Dimensions.get('window').height;
+
 export default function App() {
   return (
-    <SafeAreaView style={{flex: 1,
-                          backgroundColor:  "#EAF2F6"}}>
-    <PaperProvider theme={theme}>
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: "#EAF2F6"
+    }}>
+      <PaperProvider theme={theme}>
         <View
-        style={{
-          flex:1,
+          style={{
+            flex: 1,
 
-        }}>
-          <Header 
-          tittle={"Perfil"}
-          typeButton={"menu"}
+          }}>
+          <Header
+            tittle={"Perfil"}
+            typeButton={"menu"}
           />
           <View style={{
             flex: 1,
             height: winHeight * 0.8,
           }}>
-            <Profile/>
+            <ProfileScreen />
           </View>
-          <NavBar/>
+          <NavBar />
         </View>
-    </PaperProvider>
+      </PaperProvider>
     </SafeAreaView>
   );
 }
