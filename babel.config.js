8c42c4487d482411b41a017f217c06caafbd2a07
Thiago Toyomoto <github@thiagotoyomoto.com>
2023-11-11 16:37:23 -0300

    chore: Adicionar configurações do react-native-reanimated

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,14 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin'
+    ],
     env: {
       production: {
-        plugins: ['react-native-paper/babel'],
+        plugins: [
+          'react-native-paper/babel'
+        ],
       },
     },
   };
