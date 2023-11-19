5df715db219ff782343d9028678c01695bdfed88
Thiago Toyomoto <github@thiagotoyomoto.com>
2023-11-19 19:42:16 -0300

    chore: Renomear arquivos das telas de sign-in

--- src/screens/index.js
+++
@@ -1,7 +0,0 @@
-export * from './LoginScreen';
-export * from './ProfileScreen';
-export * from './RegisterProfileScreen';
-export * from './RegisterUserScreen';
-export *  from './EditProfileScreen';
-export * from  './HomeScreen';
-export * from './MedicationListScreen'
---
+++ src/screens/index.ts
@@ -0,0 +1,7 @@
+export * from './EditProfileScreen';
+export * from './HomeScreen';
+export * from './MedicationListScreen'
+export * from './ProfileScreen';
+export * from './SignInProfileScreen';
+export * from './SignInUserScreen';
+export * from './SignUpScreen';
